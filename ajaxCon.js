fc8e80eee83341db6741ca868b803f69ba7e4665
lo222fj <lo222fj@student.lnu.se>
2013-12-30 21:40:19 +0100

    Sprint 1 Dag 3-Ajax, tumnaglar visas, viss style
    Bilderna hämtade, tumnagelbilder visas men är ännu ej klickbara, viss
    styling

---
+++ ajaxCon.js
@@ -0,0 +1,40 @@
+function AjaxCon(url, callback) {
+    //Konstanter för status på anrop. Behövs ej. För att jag ska förstå och kunna följa
+    var READY_STATE_UNINITIALIZED = 0;
+    var READY_STATE_OPENED = 1;
+    var READY_STATE_SENT = 2;
+    var READY_STATE_LOADING = 3;
+    var READY_STATE_COMPLETE = 4;
+
+    var xhr = this.getXHR();
+    
+    xhr.onreadystatechange = function(){
+        
+        if(xhr.readyState === READY_STATE_COMPLETE){
+            if(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304){
+                callback(xhr.responseText);
+            }
+            else{
+                console.log("Läsfel, status: "+xhr.status);
+            }
+        }
+    };
+    
+    xhr.open("get", url, true);
+    
+    xhr.send(null);
+}
+
+AjaxCon.prototype.getXHR = function() {
+    var xhr = null;
+    try {
+        xhr = new XMLHttpRequest();
+    } catch (error) {
+        try {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (error) {
+            throw new Error("No XHR object available");
+        }
+    }
+    return xhr;
+}
--- application.js
+++ application.js
@@ -6,21 +6,42 @@ var backgroundChanger = {
 
         console.log(aOpenWindow);
         aOpenWindow.click(function() {
+            //skapar element för fönster och dess delar
             var divChooseBackground = $("<div id='chooseBackgroundWindow'></div>");
             var headerChooseBackground = $("<header class='headerChooseBackgroundWindow'></header>");
             var mainChooseBackground = $("<main class ='mainChooseBackgroundWindow'></main>");
             var footerChooseBackground = $("<footer class='footerChooseBackgroundWindow'></footer>");
             var iconChooseBackground = $("<img class='iconChooseBackgroundWindow' src='icon_DSC00846.png'/>");
             var headerTextChooseBackground = $("<span class='headerTextChoosenBackgroundWindow'>Välj bakgrundsbild</span>");
-  
-            $("main").append(divChooseBackground);
 
+            //lägger ut elementen i DOMen  
+            $("main").append(divChooseBackground);
             divChooseBackground.append(headerChooseBackground);
             headerChooseBackground.append(iconChooseBackground);
             headerChooseBackground.append(headerTextChooseBackground);
             divChooseBackground.append(mainChooseBackground);
             divChooseBackground.append(footerChooseBackground);
+            
+            var callback = function(respons){
+                var jsonStr = respons;
+                var images = JSON.parse(jsonStr);
+                var i;
+                
+                for(i = 0; i<images.length; i +=1){
+                var thumbSrc = images[i].thumbURL;
+                console.log(thumbSrc);
+                var img = $("<img class='imgsToChoose' src='"+thumbSrc+"' />");
+                
+                var thumbDiv = $("<div class='imageDivs'></div>");
+                thumbDiv.append(img);
+                mainChooseBackground.append(thumbDiv);
+                }
+                
+             };
+            
+            new AjaxCon("http://homepage.lnu.se/staff/tstjo/labbyServer/imgviewer/", callback);
         });
+        
     }
 };
 window.onload = function() {
